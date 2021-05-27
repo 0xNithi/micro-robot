61e7baf1acff873a7ad20d5cdf1b86325727ef05
MrNithi <bossrocker777@gmail.com>
2021-05-28 00:47:46 +0700

    Update rotate function

--- controller.py
+++ controller.py
@@ -46,10 +46,6 @@ class controller:
 
         # Init defualt variable
         self.ballStorage = []
-        self.location = {
-            "from": "greenStaion",
-            "to": "redStation"
-        }
 
         logging.info("finished initialization")
 
@@ -61,10 +57,17 @@ class controller:
 
     def goto(self, to, From):
         if to == "redStation" and From == "greenStaion":
-            #self.send("CMD:MLT:30:120")
-            #self.sendAndWait(["CMD:MBT:30:60", "GET:DC_ISIDLE"], "1\r\n")
-            #self.moveCenter()
-            self.rotateCenter()
+            self.send("CMD:MLT:30:120")
+            self.sendAndWait(["CMD:MBT:30:60", "GET:DC_ISIDLE"], "1\r\n")
+        elif to == "blueStation" and From == "redStation":
+            self.send("CMD:MBT:30:60")
+            self.sendAndWait(["CMD:MLT:30:120", "GET:DC_ISIDLE"], "1\r\n")
+            self.sendAndWait(["CMD:RLT:30:90", "GET:DC_ISIDLE"], "1\r\n")
+        elif to == "redStation" and From == "blueStation":
+            self.sendAndWait(["CMD:MBT:30:180", "GET:DC_ISIDLE"], "1\r\n")
+            self.sendAndWait(["CMD:RRT:30:90", "GET:DC_ISIDLE"], "1\r\n")
+        self.moveCenter()
+        self.rotate()
 
     def moveCenter(self):
         isCenter = False
@@ -74,50 +77,51 @@ class controller:
             line_get_main_features()
             line_get_vectors(1, vectors)
             deltaX = vectors[0].m_x1 - vectors[0].m_x0
-            print('[VECTOR: INDEX=%d X0=%d Y0=%d X1=%d Y1=%d]' % (
-                vectors[0].m_index, vectors[0].m_x0, vectors[0].m_y0, vectors[0].m_x1, vectors[0].m_y1))
 
+            # Move it left or right until the line is centered on the camera
             if abs(deltaX) == 0:
                 isCenter = True
             elif deltaX < 0:
                 self.send("CMD:MRT:30:0.5:RLS")
             else:
                 self.send("CMD:MLT:30:0.5:RLS")
-                
-    def rotateCenter(self):
+
+    def rotate(self):
         intersections = IntersectionArray(1)
-        
-        while True:
-        #    self.send("CMD:MFC:15")
+        vectors = VectorArray(4)
+        isFound = False
+
+        self.send("CMD:MFC:15")
+
+        # Find an intersection
+        while not isFound:
             line_get_all_features()
             line_get_intersections(1, intersections)
-            print('[INTERSECTION: X=%d Y=%d BRANCHES=%d]' % (intersections[0].m_x, intersections[0].m_y, intersections[0].m_n))
+
+            # If an intersection is found, stop the motor
             if intersections[0].m_y >= 15 and intersections[0].m_n >= 3:
                 self.send("CMD:STP")
-        #        self.send("CMD:MBT:30:5")
-                for lineIndex in range (0, intersections[0].m_n):
-                    print('  [LINE: INDEX=%d ANGLE=%d]' % (intersections[0].getLineIndex(lineIndex), intersections[0].getLineAngle(lineIndex)))
-                break
-        
-        while True:
-            lineAngle = []
-            index = 0
-            while True:
+                self.send("CMD:MBT:30:5")       # Move back for analyze intersection branches
+                isFound = True
+
+        isCenter = False
+
+        while not isCenter:
+            isFound = False
+            # Get an intersection
+            while not isFound:
                 line_get_all_features()
                 line_get_intersections(1, intersections)
-                print('[INTERSECTION: X=%d Y=%d BRANCHES=%d]' % (intersections[0].m_x, intersections[0].m_y, intersections[0].m_n))
+                line_get_vectors(4, vectors)
+                print('[INTERSECTION: X=%d Y=%d BRANCHES=%d]' % (
+                    intersections[0].m_x, intersections[0].m_y, intersections[0].m_n))
                 if intersections[0].m_n == 4:
-                    for lineIndex in range(0, 4):
-                        lineAngle.append(abs(intersections[0].getLineAngle(lineIndex)))
-                        print('  [LINE: INDEX=%d ANGLE=%d]' % (intersections[0].getLineIndex(lineIndex), intersections[0].getLineAngle(lineIndex)))
-                    lineAngle.sort()
-                    break
-            for lineIndex in range(0, 4):
-                if lineAngle[1] == abs(intersections[0].getLineAngle(lineIndex)):
-                    index = lineIndex
-            if abs(intersections[0].getLineAngle(index)) <= 1:
-                break
-            elif intersections[0].getLineAngle(index) < 1:
+                    isFound = True
+
+            deltaY = vectors[2].m_y0 - vectors[3].m_y1
+            if deltaY == 0:
+                isCenter = True
+            elif deltaY > 0:
                 self.send("CMD:RLT:30:1:RLS")
             else:
                 self.send("CMD:RRT:30:1:RLS")
@@ -127,11 +131,10 @@ class controller:
         logging.info(f"serial: send command {cmd}")
 
         while True:
-            res = self.ser.read_until().decode("utf-8")
-            if(res):
-                print(res)
-                logging.info(f"serial: recieve message {res}")
-                return res
+            msg = self.ser.read_until().decode("utf-8")
+            if msg:
+                logging.info(f"serial: recieve message {msg}")
+                return msg
 
     def sendAndWait(self, cmd, msg):
         self.send(cmd[0])
